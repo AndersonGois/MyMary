189be3166cafcd88bd225d520477f5899b24a60a
Anderson <andersonrejane@hotmail.com>
2015-08-06 15:02:01 -0300

    Mapeamento de estado
    Mapeamento de estado e arquivo de configuração

---
+++ Domain/Mappings/EstadoMap.cs
@@ -0,0 +1,16 @@
+﻿
+using Domain.Entities;
+using FluentNHibernate.Mapping;
+
+namespace Domain.Mappings
+{
+   public sealed class EstadoMap : ClassMap<Estado>
+    {
+       public EstadoMap()
+       {
+           Id(x => x.Id);
+           Map(x => x.Descricao);
+           Map(x => x.Sigla);
+       }
+    }
+}
