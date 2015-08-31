8e057775747f87b892e294e3cf0b5212f7e406a7
Anderson <andersonrejane@hotmail.com>
2015-08-31 16:48:18 -0300

    bootstrap

---
+++ Mvc.Mary/Scripts/app/ClienteModule.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="app.js" />
+var app = angular.module("ClienteModule", []);
+app.controller('ClienteController', ['$http', function ($http) {
+    var store = this;
+   
+    
+    store.mPais = {
+        singleSelect: null,
+        multipleSelect: [],
+    };
+
+    store.mEstado = {
+        singleSelect: null,
+        multipleSelect: [],
+    };
+
+    store.mCidade = {
+        singleSelect: null,
+        multipleSelect: [],
+    };
+
+    store.forceUnknownOption = function () {
+        store.mPais.singleSelect = { "id": 2, "name": "Japão" };
+    };
+
+    $http.get("Cliente/Pais").success(function(data) {
+        store.mPais.multipleSelect = data;
+    });
+
+    store.getEstados = function (id) {
+       $http.get("Cliente/Estados/" + id).success(function (data) {
+           store.mEstado.multipleSelect = data;
+        });
+    };
+   
+    store.teste = function () {
+        console.log(store.mPais.singleSelect);
+    };
+
+    store.getCidades = function (id) {
+       $http.get("Cliente/Cidades/" + id).success(function (data) {
+           store.mCidade.multipleSelect = data;
+        });
+    };
+}]);
+
+
--- Mvc.Mary/Scripts/app/app.js
+++ Mvc.Mary/Scripts/app/app.js
@@ -1,27 +1,3 @@
-﻿/// <reference path="app.js" />
-var app = angular.module("mary", []);
-app.controller('ClienteController', ['$http', function ($http) {
-    var store = this;
-
-    $http.get("Cliente/Pais").success(function (data) {
-        store.pais = data;
-    });
-
-    store.getEstados = function (id) {
-        store.estados = [];
-        store.cidades = [];
-        $http.get("Cliente/Estados/" + id).success(function (data) {
-            store.estados = data;
-        });
-    };
-
-    
-    store.getCidades = function (id) {
-        store.cidades = [];
-        $http.get("Cliente/Cidades/" + id).success(function (data) {
-            store.cidades = data;
-        });
-     };
-}]);
-
-
+﻿
+/// <reference path="../../Views/Cliente/endereco-cliente.html" />
+var app = angular.module("mary", ["ClienteModule"]);
